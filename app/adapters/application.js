d487d51a0da416577ae0cccff6412ca47febd347
ShivonQ <realitykeeper@gmail.com>
2017-03-05 14:45:05 -0600

    Author page, auto-record reload on a timer.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,5 +6,7 @@ const{String:{pluralize, underscore}} = Ember;
 export default JSONAPIAdapter.extend({
     pathForType(type){
         return pluralize(underscore(type));
-    }
+    },
+
+
 });
--- app/models/author.js
+++ app/models/author.js
@@ -1,6 +1,11 @@
 import DS from 'ember-data';
 import Publisher from'./publisher';
+import Ember from 'ember'
+import {hasMany} from 'ember-data/relationships'
 
 export default Publisher.extend({
-  books: DS.hasMany('book')
+    books: DS.hasMany('book', {async: true}),
+    loadedAt: Ember.on('didLoad', function () {
+        this.set('loadedAt', new Date());
+    })
 });
