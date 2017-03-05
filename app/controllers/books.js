c840cb3f47dfa4edfc3553c3b479504c95e2e406
ShivonQ <realitykeeper@gmail.com>
2017-03-05 15:13:49 -0600

    Added Component extender instead of controller (due to future deprecation) and finished app.
    NEXT MOVE: FIGURE OUT HOW TO INCLUDE API DATA FROM AMAZON-BOOKS, GOOGLE-BOOKS, or GOODREADS for real ISBN's, Cover Art, and Author BIO'S

---
+++ app/controllers/books.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+const { computed } = Ember;
+
+export default Ember.Controller.extend({
+
+    queryParams: ['limit'],
+    limit: 5,
+
+    total: computed('model.meta', function() {
+        return this.get('model.meta').total;
+    }),
+    showAll:computed('total', 'model', function(){
+        //this makes 'total' into the model.length integer so ShowAll actually shows all
+        return this.get('total') > this.get('model.length');
+    })
+
+});
--- app/routes/books.js
+++ app/routes/books.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model(){
-        return this.store.findAll('book');
+    //here is the limiter component
+    queryParams:{
+      limit:{
+          refreshModel: true
+      }
+    },
+    model(params){
+        return this.store.findAll('book',params);
+    },
+    actions: {
+        showAll(){
+            const total = this.controllerFor('books').get('total');
+            this.transitionTo({queryParams: {limit: total}})
+        }
     }
 });
